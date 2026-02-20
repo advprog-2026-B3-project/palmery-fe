71ec2876b5cf7b728447aa8c999f6b36b5fbdbe6
gung16 <gung.160906@gmail.com>
2026-02-20 15:41:59 +0700

    Front End Initialization

---
+++ README.md
@@ -0,0 +1,12 @@
+# Palmery Front End
+
+This is a Next.js project
+
+## Getting Started
+
+To run the development server:
+
+```bash
+npm run dev
+```
+It should run on localhost:3000
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-3xl font-semibold text-black dark:text-zinc-50">Hello World</h1>
+    </div>
+  );
+}
